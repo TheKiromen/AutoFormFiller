61b45182e5582f28f40bb40d34ba3a2760744e95
Dominik Kruczek <TheKiromen@gmail.com>
2025-03-28 20:12:09 +0100

    Test script execution with function

--- content_scripts/input_filler.js
+++ content_scripts/input_filler.js
@@ -1,4 +1,5 @@
 (function(){
     document.body.style.border = "5px solid red";
+    document.body.style.backgroundColor = "green";
 }
 )();
--- popup/popup.js
+++ popup/popup.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
 
-function handleButtonClick() {
+async function handleButtonClick() {
   // Get text from text area
   var textArea = document.getElementById('jsonInput');
   // Convert text to JSON
@@ -12,4 +12,16 @@ function handleButtonClick() {
 
   // Write some test data to text area
   textArea.value = "Hello World";
+
+  // Get Id of current active tab
+  const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+
+  await browser.scripting.executeScript({
+    target: { tabId: tab.id },
+    // TODO: Why is this not working?
+    // files : ["content_scripts/input_filler.js"]
+    func: () => {
+      document.body.style.border = "5px solid red";
+    }
+  });
 }
