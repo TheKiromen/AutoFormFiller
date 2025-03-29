8e8180d0f7cc82c7a44ea4a2930ae075365a774d
Dominik Kruczek <TheKiromen@gmail.com>
2025-03-29 19:36:15 +0100

    Verify that addon can invoke buttons on website

--- content_scripts/input_filler.js
+++ content_scripts/input_filler.js
@@ -1,12 +1,14 @@
 (function(){
     // Get google search input field
     const inputField = document.getElementById('APjFqb');
+    const submitButton = document.querySelector('input[type="submit"]');
     
-    if(inputField === null){
+    if(inputField === null || submitButton === null) {
         console.log("Input field not found");
         return;
     }
 
     inputField.value = "Hello Wrold!";
+    submitButton.click();
 }
 )();
