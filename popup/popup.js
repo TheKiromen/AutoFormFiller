6edceed3990920a9541d7cf78f749f3f9be3ab1d
Dominik Kruczek <TheKiromen@gmail.com>
2025-03-31 18:23:46 +0200

    Add validation of input JSON

--- popup/popup.js
+++ popup/popup.js
@@ -4,21 +4,43 @@ document.addEventListener('DOMContentLoaded', () => {
   
 
 async function handleButtonClick() {
-  // Get text from text area
+  var tabsData = [];
   var textArea = document.getElementById('jsonInput');
-  // Convert text to JSON
-  var json = JSON.parse(textArea.value);
 
-  // Get Id of current active tab
-  const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+  try {
+    var json = JSON.parse(textArea.value);
+    // Input should be an array of objects
+    for (var i = 0; i < json.length; i++) {
+      var tabData = new TabData("https://www.google.pl", json[i].query);
+      tabsData.push(tabData);
+      console.log(tabData);
+    }
+  }
+  catch (e) {
+    alert('Invalid JSON, please check your input.');
+    return;
+  }
 
-  await browser.scripting.executeScript({
-    target: { tabId: tab.id },
-    files : ["../content_scripts/input_filler.js"],
-  });
+  // // Get Id of current active tab
+  // const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
 
-  browser.tabs.sendMessage(tab.id, {
-    command: 'fillInputFields',
-    data: json,
-  });
+  // await browser.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   files : ["../content_scripts/input_filler.js"],
+  // });
+
+  // browser.tabs.sendMessage(tab.id, {
+  //   command: 'fillInputFields',
+  //   data: json,
+  // });
+}
+
+class TabData {
+  constructor(url, title) {
+    if(!url || !title) {
+      throw new Error('URL and title are required');
+    }
+    this.url = url;
+    this.title = title;
+  }
 }
